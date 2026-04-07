3978439e57a1255bda64f87eb4f2548063a08ece
mitama <nigimitama16@gmail.com>
2026-04-07 20:50:38 +0900

    ci: MacOSでは.appファイルにする必要があるようなので修正

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e
 
-uv run --with pyinstaller pyinstaller \
-  --windowed \
-  --onefile \
-  --noconfirm \
-  --name pdf-ebook-maker \
-  --paths src \
-  --add-data "src/ocr/model:ocr/model" \
-  --add-data "src/ocr/config:ocr/config" \
-  --add-data "images/pdfbook.ico:images" \
-  --icon "images/pdfbook.ico" \
-  src/main.py
+args=(
+  --windowed
+  --noconfirm
+  --name pdf-ebook-maker
+  --paths src
+  --add-data "src/ocr/model:ocr/model"
+  --add-data "src/ocr/config:ocr/config"
+  --add-data "images/pdfbook.ico:images"
+  --icon "images/pdfbook.ico"
+)
+
+# MacOSの場合は--onefileじゃなく--onedir（デフォルト値）でビルドして.appファイルにさせる
+[[ "$(uname -s)" != "Darwin" ]] && args+=(--onefile)
+
+uv run --with pyinstaller pyinstaller "${args[@]}" src/main.py
